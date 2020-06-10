97da91d4f5e5bd587be16a3105ec9ffd7a0785a8
vishal jangid <jangidvishal1999@gmail.com>
2020-06-10 12:11:10 +0530

    Material ui added

--- client/src/router/router.js
+++ client/src/router/router.js
@@ -1,1 +1,15 @@
-import React form 'react';
+import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { Home } from "../pages/home"
+
+export class AppRouter extends React.Component {
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path='/' component={Home}></Route>
+                </Switch>
+            </Router>
+        )
+    }
+}
