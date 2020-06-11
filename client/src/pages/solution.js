3438e0ff8b7723cccd7f245e8ad8c1dea532f568
vishal jangid <jangidvishal1999@gmail.com>
2020-06-11 22:09:55 +0530

    Algorithm implemnted Successful

--- client/src/pages/solution.js
+++ client/src/pages/solution.js
@@ -6,8 +6,8 @@ import { Link } from 'react-router-dom'
 const Solution = (props) => {
   const data = dataForAlgo(props.location.state.data)
 
-  // console.log(calculate(data.rod_length, data.kerf, data.iteams));
-  calculate(data.rod_length, data.kerf, data.iteams)
+  console.log(calculate(data.rod_length, data.kerf, data.iteams))
+  // calculate(data.rod_length, data.kerf, data.iteams)
 
   return <Link to="/"> Back</Link>
 }
@@ -17,7 +17,7 @@ const dataForAlgo = (data) => {
   const kerf = data[0].kerf
   let iteams = []
   for (let i = 0; i < data[1].length; i++) {
-    iteams.push({ length: data[1][i].len, quantity: data[1][i].quantity })
+    iteams.push({ len: data[1][i].len, quantity: data[1][i].quantity })
   }
   return {
     rod_length,
