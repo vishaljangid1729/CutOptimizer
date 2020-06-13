3248ee84817ce1d25bbb3499182816b5db190c90
vishal jangid <jangidvishal1999@gmail.com>
2020-06-13 19:44:46 +0530

    csv router added

--- src/router/router.js
+++ src/router/router.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Home } from '../pages/home'
 import { Solution } from '../pages/solution'
 import { ManualEntry } from '../pages/manual_entry'
+import { CSVEntry } from '../pages/csv_entry'
 
 export class AppRouter extends React.Component {
   render() {
@@ -13,6 +14,9 @@ export class AppRouter extends React.Component {
         </Switch>
         <Switch>
           <Route exact path="/manual" component={ManualEntry}></Route>
+        </Switch>
+        <Switch>
+          <Route exact path="/csv-entry" component={CSVEntry}></Route>
         </Switch>
         <Switch>
           <Route exact path="/solution" component={Solution}></Route>
